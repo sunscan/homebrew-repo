c1e04c191807cb9177798958bb54b8923b86cd68
sunscan <sunscan@gmail.com>
2020-02-03 15:12:06 +0100

    Update Burp Suite Professional to 2020.1

--- Casks/burp-suite-pro.rb
+++ Casks/burp-suite-pro.rb
@@ -1,6 +1,6 @@
 cask 'burp-suite-pro' do
-  version '2.1.05'
-  sha256 'f2105ec4fd4ba8ff8d8f0ee295fe87be15703244fa3304b9af7c54d7807dbc12'
+  version '2020.1'
+  sha256 '5daee0365651926499bb319931e9dd64c83d84c4349b15a2ffb34a8368d56a76'
 
   url "file://#{ENV['HOME']}/Downloads/burpsuite_pro_macos_v#{version.dots_to_underscores}.dmg"
   appcast 'http://releases.portswigger.net/'
