d134cc769d7c0f5dbe77be67c523345cb8344b60
sunscan <sunscan@gmail.com>
2020-08-02 19:33:00 +0200

    Update Burp Suite Professional to 2020.7

--- Casks/burp-suite-pro.rb
+++ Casks/burp-suite-pro.rb
@@ -1,15 +1,21 @@
 cask 'burp-suite-pro' do
-  version '2020.5.1'
-  sha256 'd3c5b15675cd6d8bec424ff7989c30263a825af3ef2e91adeaa6b1b98c9c70c8'
+  version '2020.7'
+  sha256 '54a023714f6d36cd498c743c86264e1aa91503b4a68fa7c1b76a0bd408613294'
 
   url "file://#{ENV['HOME']}/Downloads/burpsuite_pro_macos_v#{version.dots_to_underscores}.dmg"
   appcast 'http://releases.portswigger.net/'
   name 'Burp Suite Professional'
   homepage 'https://portswigger.net/burp/'
 
+  preflight do
+    system_command '/usr/bin/xattr',
+                   args: ['-rd', 'com.apple.quarantine', "#{staged_path}/Burp Suite Professional Installer.app"],
+                   sudo: true
+  end
+
   installer script: {
                       executable: 'Burp Suite Professional Installer.app/Contents/MacOS/JavaApplicationStub',
-                      args:       ['-q', '-overwrite', '-dir', '/Applications/'],
+                      args:       ['-q'],
                       sudo:       true,
                     }
 
