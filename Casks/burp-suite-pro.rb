8293d79019d6ab3d7e469918cc150e0bb0adf6d7
sunscan <sunscan@gmail.com>
2021-05-04 12:25:13 +0200

    Update Burp Suite Professional to 2021.4.3

--- Casks/burp-suite-pro.rb
+++ Casks/burp-suite-pro.rb
@@ -1,6 +1,6 @@
 cask 'burp-suite-pro' do
-  version '2021.3.3'
-  sha256 '2721a77e147df0102397458fc70465a1a368826e04c7e34f2dbc14f44367bf27'
+  version '2021.4.3'
+  sha256 '7dd3464bcf6e2bff7d55045645a405a829b245634d1759234fbcd5d9bad08060'
 
   url "file://#{ENV['HOME']}/Downloads/burpsuite_pro_macos_v#{version.dots_to_underscores}.dmg"
   appcast 'http://releases.portswigger.net/'
